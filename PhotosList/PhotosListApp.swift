49e4036983837320363e2eef4ef7ff97506b547c
Mathan Rajan <mathanrajan@calibraint.com>
2023-08-21 18:41:34 +0530

    Initial Commit

---
+++ PhotosList/PhotosListApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotosListApp.swift
+//  PhotosList
+//
+//  Created by Mathan Rajan J on 21/08/23.
+//
+
+import SwiftUI
+
+@main
+struct PhotosListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
